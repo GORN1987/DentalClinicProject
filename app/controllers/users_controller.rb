f5f92793895f5b63255dd0771d5a1cf8a20ee4e1
GORN1987 <guilhermepmarinho@gmail.com>
2018-05-12 23:43:08 +0100

    add validation and styles to users crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,15 +24,23 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
+    debugger
     @user = User.new(user_params)
+    if (verifyDuplicatedLogin(@user.login))
+      respond_to do |format|
+        flash[:message] = "The chosen  login already exist"
+        format.html {render :new}
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    else
+      respond_to do |format|
+        if @user.save
+          format.html {redirect_to @user, notice: 'User was successfully created.'}
+          format.json {render :show, status: :created, location: @user}
+        else
+          format.html {render :new}
+          format.json {render json: @user.errors, status: :unprocessable_entity}
+        end
       end
     end
   end
@@ -40,15 +48,42 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+    if (verifyDuplicatedLoginUpdate(@user.login, @user.id))
+      respond_to do |format|
+        flash[:message] = "The choose login already exist"
+        format.html {render :new}
+
+      end
+    else
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html {redirect_to @user, notice: 'User was successfully updated.'}
+          format.json {render :show, status: :ok, location: @user}
+        else
+          format.html {render :edit}
+          format.json {render json: @user.errors, status: :unprocessable_entity}
+        end
       end
     end
+  end
+
+  def verifyDuplicatedLogin(login)
+    if User.where(["login = ?", login]).length > 0
+      return true
+    else
+      return false
+    end
+
+  end
+
+
+  def verifyDuplicatedLoginUpdate(login, id)
+    if User.where(["login = ? and id <> ?", login, id]).length > 0
+      return true
+    else
+      return false
+    end
+
   end
 
   # DELETE /users/1
@@ -56,19 +91,20 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to users_url, notice: 'User was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :login, :password,:password_confirmation , :login_type, :email, :phone, :role)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:name, :login, :password, :password_confirmation, :login_type, :email, :phone, :role)
+  end
 end
